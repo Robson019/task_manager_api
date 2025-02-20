ab69a89ff81c809e5b898b15408443e4725ab0b8
Robson019 <rag2@aluno.ifal.edu.br>
2025-02-20 17:33:46 -0300

    fix migrations again again

--- src/api/app/start.sh
+++ src/api/app/start.sh
@@ -1,10 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 # Run application migrations
-if [ -n "$DATABASE_SCHEMA" ] && [ -n "$DATABASE_MIGRATIONS_PATH" ]; then
+if [[ $DATABASE_SCHEMA != "" && $DATABASE_MIGRATIONS_PATH != "" ]]; then
   echo "Searching for migrations..."
   uri="$DATABASE_SCHEMA://$DATABASE_USER:$DATABASE_PASSWORD@$DATABASE_HOST:$DATABASE_PORT/$DATABASE_NAME?sslmode=$DATABASE_SSL_MODE"
-  migrate -path "$DATABASE_MIGRATIONS_PATH" -database "$uri" up
+  migrate -path $DATABASE_MIGRATIONS_PATH -database $uri up
 fi
 
 # Run application binary
